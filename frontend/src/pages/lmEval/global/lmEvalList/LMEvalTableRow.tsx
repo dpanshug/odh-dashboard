44489c6ee2c784d7c3dcbed4b34f8735179bced0
Emmanuel Ikeola <eikeola@redhat.com>
2025-06-11 13:43:11 +0100

    reverted lmeval tablerow file

--- frontend/src/pages/lmEval/global/lmEvalList/LMEvalTableRow.tsx
+++ frontend/src/pages/lmEval/global/lmEvalList/LMEvalTableRow.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { ActionsColumn, Td, Tr } from '@patternfly/react-table';
-import { Button, Timestamp } from '@patternfly/react-core';
-import { useNavigate } from 'react-router';
+import { Timestamp } from '@patternfly/react-core';
 import { LMEvalKind } from '#~/k8sTypes';
 import { downloadString } from '#~/utilities/string';
 import LMEvalStatus from './LMEvalStatus';
@@ -11,7 +10,6 @@ type LMEvalTableRowType = {
 };
 
 const LMEvalTableRow: React.FC<LMEvalTableRowType> = ({ lmEval }) => {
-  const navigate = useNavigate();
   const handleDownload = () => {
     const rawData = JSON.stringify(lmEval);
     downloadString(`${lmEval.metadata.name}.json`, rawData);
@@ -19,17 +17,7 @@ const LMEvalTableRow: React.FC<LMEvalTableRowType> = ({ lmEval }) => {
 
   return (
     <Tr>
-      <Td dataLabel="Evaluation">
-        <Button
-          variant="link"
-          data-testid="lm-evaluation-cancel-button"
-          onClick={() => {
-            navigate(`result/${lmEval.metadata.name}`);
-          }}
-        >
-          {lmEval.metadata.name}{' '}
-        </Button>
-      </Td>
+      <Td dataLabel="Evaluation">{lmEval.metadata.name}</Td>
       <Td dataLabel="Model">
         {lmEval.spec.modelArgs?.find((arg) => arg.name === 'model')?.value || '-'}
       </Td>
